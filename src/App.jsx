34b06a33f0639d2a5a4fdab0ba7e9ee305e4ec4c
Alex3126q <kornievskijj@gmail.com>
2025-07-10 17:58:55 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,13 @@
+import Hero from "./components/Hero";
+
+function App() {
+  return (
+    <div className="bg-amber-50 min-h-screen text-gray-800">
+      <Hero />
+      {/* Тут буде решта сайту: товари, форма, кошик */}
+    </div>
+  );
+}
+
+export default App;
 
